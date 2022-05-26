023c4730a2480be5ab2d1f1114988360cc3aa175
Gustavo <gustavo28064212@gmail.com>
2022-05-26 13:28:56 -0300

    add fake data

---
+++ src/main/java/CarRental/FakeData.java
@@ -0,0 +1,34 @@
+package CarRental;
+
+import CarRental.domain.Car;
+import CarRental.domain.Client;
+import CarRental.repositories.CarRepository;
+import CarRental.repositories.UserRepository;
+
+public class FakeData {
+
+    public static void addClients(){
+        Client gustavo = new Client("gustavo", "1234");
+        UserRepository.addUser(gustavo);
+        Client felipe = new Client("felipe", "1234");
+        UserRepository.addUser(felipe);
+        Client kaique = new Client("kaique", "1234");
+        UserRepository.addUser(kaique);
+        Client lucas = new Client("lucas", "1234");
+        UserRepository.addUser(lucas);
+        Client gabriel = new Client("gabriel", "1234");
+        UserRepository.addUser(gabriel);
+
+    }
+
+    public static void addCars(){
+        Car ferrari = new Car("Ferrari", "F-50", "abc1234", 5000);
+        CarRepository.addCar(ferrari);
+        Car palio = new Car("Fiat", "Palio", "abc2345", 100);
+        CarRepository.addCar(palio);
+        Car pegeout = new Car("Pegeout", "206", "abc3456", 150);
+        CarRepository.addCar(pegeout);
+
+    }
+
+}
